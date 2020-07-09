43a2423843fdaa8a48269651a6a3512d985819a3
Hrishikesh Nikam <hrishinikam2000@gmail.com>
2020-07-09 12:44:36 +0530

    update python file

--- face_recegnition.py
+++ face_recegnition.py
@@ -4,3 +4,21 @@ import cv2
 import face_recognition
 import numpy as np
 from time import sleep
+
+def get_encoded_faces():
+    """
+    looks through the faces folder and encodes all
+    the faces
+e
+    :return: dict of (name, image encoded)
+    """
+    encoded = {}
+
+    for dirpath, dnames, fnames in os.walk("./faces"):
+        for f in fnames:
+            if f.endswith(".jpg") or f.endswith(".png"):
+                face = fr.load_image_file("faces/" + f)
+                encoding = fr.face_encodings(face)[0]
+                encoded[f.split(".")[0]] = encoding
+
+    return encoded
